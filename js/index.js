8433ab3b28f4687a21a080e8571cf2b8af347689
Serg Klukvin <klukvin@ukr.net>
2020-05-21 12:13:55 +0300

    end

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function hook(e) {
+  var el = document.getElementById("div_main");
+  el.startX = e.type == "mousedown" ? e.clientX - el.offsetWidth : 0;
+  el.startY = e.type == "mousedown" ? e.clientY - el.offsetHeight : 0;
+}
+
+function move(e) {
+  var el = document.getElementById("div_main");
+
+  if (el.startX) el.style.width = e.clientX - el.startX + "px";
+
+  if (el.startY) el.style.height = e.clientY - el.startY + "px";
+
+  e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+}
+
+if (!document.attachEvent) {
+  document.attachEvent = function (e, f) {
+    this.addEventListener(e.substr(2), f, false);
+  };
+}
+document.attachEvent("onmouseup", hook);
+document.attachEvent("onmousemove", move);
